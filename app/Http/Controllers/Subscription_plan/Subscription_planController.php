fa7525000e908105150f7efe07d2cbf475a80a23
Richar2 <richard.coimec91@hotmail.com>
2019-05-10 16:13:17 -0300

    criacao_classes

---
+++ app/Http/Controllers/Subscription_plan/Subscription_planController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Subscription_plan;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class Subscription_planController extends Controller
+{
+    public function home()
+    {
+        return view('public_template.home');  
+    }
+    
+    
+
+
+
+
+
+
+
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'Subscription_plan\Subscription_planController@home' );
