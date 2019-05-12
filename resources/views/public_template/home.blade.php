87691c08dcbab05d2a7b5769598cc567b35e8f59
Richar2 <richard.coimec91@hotmail.com>
2019-05-12 14:18:59 -0300

    novos arquivos

--- resources/views/public_template/home.blade.php
+++ resources/views/public_template/home.blade.php
@@ -6,13 +6,19 @@
     <link rel="stylesheet" href="{{asset ('fontawesome-free-5.8.2-web/css/all.min.css')}}">
     <link rel="stylesheet" href="{{asset ('css/app.css')}}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Clube Rural</title>
 </head>
 <body>
         <h1><i class="fab fa-accessible-icon"></i>hello</h1>
-
-
-
+        <select class="form-control form-control-lg">
+  <option>Large select</option>
+</select>
+<select class="form-control">
+  <option>Default select</option>
+</select>
+<select class="form-control form-group col-md-6">
+  <option>Small select</option>
+</select>
 
         <script src="{{asset ('js/app.js')}}"></script>
 </body>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.scripts(['resources/bootstrap/dist/js/bootstrap.js',
-], 'public/js/app.js');
+mix.scripts(['node_modules/bootstrap/dist/js/bootstrap.js',
+            'node_modules/jquery/dist/jquery.js',
+            'node_modules/popper.js/dist/popper.js',  
+
+        ], 'public/js/app.js')
+    .sass('resources/sass/app.scss', 'public/css/app.css');
