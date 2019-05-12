0119ba04320acc27d2ecd3371ad2df76c388b4dd
Richar2 <richard.coimec91@hotmail.com>
2019-05-12 09:32:57 -0300

    atualizacao_html

--- resources/views/public_template/home.blade.php
+++ resources/views/public_template/home.blade.php
@@ -3,10 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset ('fontawesome-free-5.8.2-web/css/all.min.css')}}">
+    <link rel="stylesheet" href="{{asset ('css/app.css')}}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
 <body>
-        
+        <h1><i class="fab fa-accessible-icon"></i>hello</h1>
+
+
+
+
+        <script src="{{asset ('js/app.js')}}"></script>
 </body>
 </html>
