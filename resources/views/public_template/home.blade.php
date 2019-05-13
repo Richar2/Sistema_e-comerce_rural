d8cbc6f9627de26e57377a1ec79d849789a2a315
Richar2 <richard.coimec91@hotmail.com>
2019-05-13 19:58:21 -0300

    atu pc1

--- resources/views/public_template/home.blade.php
+++ resources/views/public_template/home.blade.php
@@ -5,12 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{asset ('fontawesome-free-5.8.2-web/css/all.min.css')}}">
     <link rel="stylesheet" href="{{asset ('css/app.css')}}">
+    <link rel="stylesheet" href="{{asset ('css/temple1.css')}}">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,900" rel="stylesheet"> 
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Clube Rural</title>
 </head>
 <body>
+    <nav id="nav_temple1" >
+        <ul class="position_menu ">
+            <div class="menu">
+                <li class="nav_int1"><a href="#">Início</a></li>
+                <li class="nav_int2"><a href="#">Entrar</a></li>
+                <li class="nav_int3"><a href="#">Novo cadastro</a></li>
+                <li class="nav_int4"><a href="#">Publicar Anúncio</a></li>
+            </div>     
+            <div class="icon_position">
+                <li><i class="fas fa-search"></i></li>
+                <li><i class="fas fa-shopping-bag"><i class="fas fa-circle">5</i></i></li>
+            </div>    
+        </ul>
+        
+    </nav>
 
-
+           
         <script src="{{asset ('js/app.js')}}"></script>
 </body>
 </html>
